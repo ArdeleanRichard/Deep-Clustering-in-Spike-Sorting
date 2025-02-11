97c79b44e7294bff76a53336070f28c18809c185
Richard Ardelean <ardeleaneugenrichard@gmail.com>
2025-02-11 16:46:23 +0200

    run deep algs

--- gs_algos.py
+++ gs_algos.py
@@ -35,25 +35,27 @@ def load_algorithms():
         #
         #     },
         # },
-        "aec": {
-            "estimator": AEC,
-            "param_grid": {
-                "n_clusters": [2],
-                "embedding_size": [10],
-                "pretrain_optimizer_params": [{"lr": 1e-2}],
-                "clustering_optimizer_params": [{"lr": 1e-4}],
-                # "pretrain_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
-                # "clustering_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
-                "random_state": [42]
-            },
-        },
+        # "aec": {
+        #     "estimator": AEC,
+        #     "param_grid": {
+        #         "n_clusters": [2],
+        #         "embedding_size": [10],
+        #         "pretrain_optimizer_params": [{"lr": 1e-2}],
+        #         "clustering_optimizer_params": [{"lr": 1e-4}],
+        #         # "pretrain_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
+        #         # "clustering_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
+        #         "random_state": [42]
+        #     },
+        # },
         # "dcn": {
         #     "estimator": DCN,
         #     "param_grid": {
         #         "n_clusters": [2],
         #         "embedding_size": [10],
-        #         "pretrain_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
-        #         "clustering_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
+        #         "pretrain_optimizer_params": [{"lr": 1e-2}],
+        #         "clustering_optimizer_params": [{"lr": 1e-3}],
+        #         # "pretrain_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
+        #         # "clustering_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
         #         "random_state": [42]
         #     },
         # },
@@ -98,16 +100,18 @@ def load_algorithms():
         #         "random_state": [42]
         #     },
         # },
-        # "dipencoder": {
-        #     "estimator": DipEncoder,
-        #     "param_grid": {
-        #         "n_clusters": [2],
-        #         "embedding_size": [10],
-        #         "pretrain_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
-        #         "clustering_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
-        #         "random_state": [42]
-        #     },
-        # },
+        "dipencoder": {
+            "estimator": DipEncoder,
+            "param_grid": {
+                "n_clusters": [2],
+                "embedding_size": [10],
+                "pretrain_optimizer_params": [{"lr": 1e-3}],
+                "clustering_optimizer_params": [{"lr": 1e-4}],
+                # "pretrain_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
+                # "clustering_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
+                "random_state": [42]
+            },
+        },
         # "dkm": {
         #     "estimator": DKM,
         #     "param_grid": {
