5e37305c592fd3af3c98c7007bdec9bdcdf2e21f
eardelean <E.Ardelean@stratec.com>
2025-05-14 10:51:03 +0300

    vade trial

--- gs_algos.py
+++ gs_algos.py
@@ -45,18 +45,18 @@ def load_algorithms():
         #
         #     },
         # },
-        "aec": {
-            "estimator": AEC,
-            "param_grid": {
-                "n_clusters": [2],
-                "embedding_size": [10],
-                "pretrain_optimizer_params": [{"lr": 1e-5}],
-                "clustering_optimizer_params": [{"lr": 1e-2}],
-                # "pretrain_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
-                # "clustering_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
-                "random_state": [random_state]
-            },
-        },
+        # "aec": {
+        #     "estimator": AEC,
+        #     "param_grid": {
+        #         "n_clusters": [2],
+        #         "embedding_size": [10],
+        #         "pretrain_optimizer_params": [{"lr": 1e-5}],
+        #         "clustering_optimizer_params": [{"lr": 1e-2}],
+        #         # "pretrain_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
+        #         # "clustering_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
+        #         "random_state": [random_state]
+        #     },
+        # },
         # "dcn": {
         #     "estimator": DCN,
         #     "param_grid": {
@@ -179,42 +179,42 @@ def load_algorithms():
         #         "random_state": [random_state],
         #     },
         # },
-        # "vade": {
-        #     "estimator": VaDE,
-        #     "param_grid": {
-        #         "n_clusters": [2],
-        #
-        #         "embedding_size": [10],
-        #         # "neural_network": [(FeedforwardAutoencoder, {"layers": [79, 100, 100, 100, 50], "dropout": 0.5, "batch_norm": True})], # error error no sense
-        #         "pretrain_optimizer_params":    [{"lr": 1e-2}],
-        #         "clustering_optimizer_params":  [{"lr": 1e-3}],
-        #         # "batch_size": [256],
-        #         # "pretrain_epochs": [10],
-        #         # "clustering_epochs": [150],
-        #
-        #         # "pretrain_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}],
-        #         # "clustering_optimizer_params": [{"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
-        #         # "batch_size": [256, 32, 64, 128, 512],
-        #         # "pretrain_epochs": [10, 50, 100],
-        #         # "clustering_epochs": [150, 300],
-        #         "random_state": [random_state],
-        #
-        #         # "initial_clustering_class": [KMeans],
-        #         # "initial_clustering_params": [{}],
-        #
-        #         # "neural_network": [neural_network],
-        #         # "embedding_size": [10, 20, 30, 40, 50, 60, 70],
-        #
-        #         # "embedding_size": [10],
-        #         # "clustering_loss_weight": [1.0],
-        #         # "ssl_loss_weight": [0.1],
-        #
-        #         # "embedding_size": [10],
-        #         # "batch_size": [1024],
-        #         # "clustering_loss_weight": [0.1],
-        #         # "ssl_loss_weight": [1.0],
-        #     },
-        # },
+        "vade": {
+            "estimator": VaDE,
+            "param_grid": {
+                "n_clusters": [2],
+
+                "embedding_size": [10],
+                # "neural_network": [(FeedforwardAutoencoder, {"layers": [79, 100, 100, 100, 50], "dropout": 0.5, "batch_norm": True})], # error no sense
+                # "pretrain_optimizer_params":    [{"lr": 1e-2}],
+                # "clustering_optimizer_params":  [{"lr": 1e-3}],
+                # "batch_size": [256],
+                # "pretrain_epochs": [10],
+                # "clustering_epochs": [150],
+
+                "pretrain_optimizer_params": [{"lr": 1e-2}, {"lr": 1e-3}, {"lr": 1e-4}],
+                "clustering_optimizer_params": [{"lr": 1e-3}, {"lr": 1e-4}, {"lr": 1e-5}],
+                # "batch_size": [256, 32, 64, 128, 512],
+                # "pretrain_epochs": [10, 50, 100],
+                # "clustering_epochs": [150, 300],
+                "random_state": [random_state],
+
+                # "initial_clustering_class": [KMeans],
+                # "initial_clustering_params": [{}],
+
+                # "neural_network": [neural_network],
+                # "embedding_size": [10, 20, 30, 40, 50, 60, 70],
+
+                # "embedding_size": [10],
+                # "clustering_loss_weight": [1.0],
+                # "ssl_loss_weight": [0.1],
+
+                # "embedding_size": [10],
+                # "batch_size": [1024],
+                # "clustering_loss_weight": [0.1],
+                # "ssl_loss_weight": [1.0],
+            },
+        },
 
 
         # "autoclustering": {
--- gs_datasets.py
+++ gs_datasets.py
@@ -3,8 +3,8 @@ from dataset_parsing import simulations_dataset as ds
 
 def load_all_data():
     datasets = []
-    for simulation_number in range(1, 96):
-    # for simulation_number in [2,4,8]:
+    # for simulation_number in range(1, 96):
+    for simulation_number in [1,2,4,8]:
     # for simulation_number in [30]:
         if simulation_number == 25 or simulation_number == 44 or simulation_number == 78:
             continue
